7b4e19a2c7b21ad3cd8ffc4128b3a57bf315dd1d
badri-akkala <lcb2021029@iiitl.ac.in>
2024-02-24 11:03:12 +0530

    added colorful bottom nav bar (non functional)

--- app/src/main/java/com/example/tap_target_view/MainActivity.kt
+++ app/src/main/java/com/example/tap_target_view/MainActivity.kt
@@ -7,6 +7,7 @@ import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.example.tap_target_view.databinding.ActivityMainBinding
@@ -32,14 +33,37 @@ class MainActivity : AppCompatActivity() {
                 createTarget(binding.btnSecond, "asf", "afaf")!!,
                 createTarget(binding.btnThird, "asf", "afaf")!!,
                 onFinish = {
-                    // Save in shared preferences that the target sequence has been shown
-                    sharedPref.edit().putBoolean("didShowTargetSequence", true).apply()
+                    // Save in shared preferences that the target sequence has been shown (use true)
+                    sharedPref.edit().putBoolean("didShowTargetSequence", false).apply()
                 },
                 onCancel = {}
             )
         }
 
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_item_home -> {
+                    showToast("Home Clicked")
+                    // Handle navigation or other actions for Home
+                    true
+                }
+                R.id.nav_item_search -> {
+                    showToast("Search Clicked")
+                    // Handle navigation or other actions for Search
+                    true
+                }
+                R.id.nav_item_profile -> {
+                    showToast("Profile Clicked")
+                    // Handle navigation or other actions for Profile
+                    true
+                }
+                else -> false
+            }
+        }
+    }
 
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 
 }
